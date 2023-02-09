0bd09d34697e45ca4c162194073097f5f6cb3792
batman <2advancesolutions@gmail.com>
2023-02-09 00:23:04 -0600

    updated commit

--- taxi-share/app/page.tsx
+++ taxi-share/app/page.tsx
@@ -1,6 +1,4 @@
 'use client'
-
-import Image from 'next/image'
 import HeroSection from '@/components/HeroSection'
 import Features from '@/components/Features'
 import Testimonials from '@/components/Testimonials'
